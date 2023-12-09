4e2cb4a45d03e06e646925e0d4c900fc7be0c745
Пользователь <@USER>
2023-12-09 12:32:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client {
+
+    public static void Main()
+    {
+       TcpClient tcpClient = new TcpClient();
+        Console.WriteLine("Клиент запущен");
+       tcpClient.Connect("127.0.0.1", 8888);
+        
+        if (tcpClient.Connected)
+        {
+            var stream = tcpClient.GetStream();
+            Console.WriteLine($"Подключение с {tcpClient.Client.RemoteEndPoint} установлено");
+            while (true)
+            {
+                byte[] buffer = new byte[1024]; 
+                int b = stream.Read(buffer,0,1024);
+                //for (int i = 0; i < 1024; i++)
+                //{
+                //    Console.Write((char)buffer[i]);
+                //}
+                
+                Console.WriteLine(Encoding.UTF8.GetString(buffer));
+
+            }
+        }
+        else
+            Console.WriteLine("Не удалось подключиться");
+    }
+
+}
+
